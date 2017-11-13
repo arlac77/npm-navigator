5cc900b08736f458ed60a220e9b4018ee0ca3900
Markus Felten <Markus.Felten@gmx.de>
2017-11-13 20:04:48 +0100

    fix: display dependencies as wires

--- src/main.js
+++ src/main.js
@@ -38,6 +38,7 @@ async function addModule(module, version = 'latest', depth = 1) {
 
   const panel = wiredPanels.createPanel();
   const sockets = [];
+  const wires = [];
   panel.x = 100;
   panel.y = 100;
   panel.label.textContent = module.name;
@@ -48,22 +49,29 @@ async function addModule(module, version = 'latest', depth = 1) {
   sockets.push(socket);
 
   if (latest && latest.dependencies !== undefined) {
-    Object.keys(latest.dependencies).forEach(async d => {
-      const socket = wiredPanels.createSocket();
-      socket.panel = panel;
-      socket.orientation = 'left';
-      socket.label.textContent = `${d}@${latest.dependencies[d]}`;
-      sockets.push(socket);
+    await Promise.all(
+      Object.keys(latest.dependencies).map(async d => {
+        const socket = wiredPanels.createSocket();
+        socket.panel = panel;
+        socket.orientation = 'left';
+        socket.label.textContent = `${d}@${latest.dependencies[d]}`;
+        sockets.push(socket);
 
-      if (depth > 0) {
-        const v = latest.dependencies[d];
-        const p = await loadModule(d, v, depth - 1);
-        //    socket.
-      }
-    });
+        if (depth > 0) {
+          const v = latest.dependencies[d];
+          const p2 = await loadModule(d, v, depth - 1);
+          const s2 = p2.sockets.filter(s => s.orientation === 'top')[0];
+
+          const wire = wiredPanels.createWire();
+          wire.srcSocket = socket;
+          wire.dstSocket = s2;
+          wires.push(wire);
+        }
+      })
+    );
   }
 
-  wiredPanels.changeGraphUndoable(new Set([panel, ...sockets]), []);
+  wiredPanels.changeGraphUndoable(new Set([panel, ...sockets, ...wires]), []);
 
   return panel;
 }
@@ -73,4 +81,4 @@ async function loadModule(id, version, depth) {
   return addModule(await result.json(), version, depth);
 }
 
-loadModule('config-expander', 'latest', 3);
+loadModule('config-expander', 'latest', 2);
