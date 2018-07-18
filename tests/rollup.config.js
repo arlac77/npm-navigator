c22962789138f894c138565da78a8a144b2dcb7b
Markus Felten <Markus.Felten@gmx.de>
2018-07-18 08:44:16 +0200

    chore(package): rollup@^0.63.0 docs(package): remove keyword null chore(rollup): update from template

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -9,7 +9,8 @@ export default {
   output: {
     file: 'build/bundle-test.js',
     format: 'cjs',
-    sourcemap: true
+    sourcemap: true,
+    interop: false
   },
   external: ['ava'],
   plugins: [multiEntry(), istanbul({
