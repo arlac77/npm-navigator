08447ddc4eb7ef6c9028d6fe3f8271e2d0cc92af
Markus Felten <Markus.Felten@gmx.de>
2018-07-28 10:54:34 +0200

    chore(rollup): interop: false

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,5 +1,4 @@
 import istanbul from 'rollup-plugin-istanbul';
-
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
@@ -13,7 +12,12 @@ export default {
     interop: false
   },
   external: ['ava'],
-  plugins: [multiEntry(), istanbul({
-    exclude: ['tests/**/*-test.js']
-  }), resolve(), commonjs()]
+  plugins: [
+    multiEntry(),
+    istanbul({
+      exclude: ['tests/**/*-test.js']
+    }),
+    resolve(),
+    commonjs()
+  ]
 };
