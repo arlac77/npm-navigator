be068126f365fd6705a95f54723a9bc0c120754c
Markus Felten <Markus.Felten@gmx.de>
2020-07-06 21:05:21 +0200

    fix(pacman): compress files

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -1,10 +1,20 @@
 
 post_install() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map *.svg)
 	systemctl reload nginx
 }
 
+pre_upgrade() {
+	(cd {{installdir}}; rm *.gz)
+}
+
 post_upgrade() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map *.svg)
 	systemctl reload nginx
+}
+
+pre_remove() {
+	(cd {{installdir}}; rm *.gz)
 }
 
 post_remove() {
