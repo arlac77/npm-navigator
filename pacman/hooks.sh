70b25d2e2796a0c16e485118a52ee7d0cc16bf3a
Markus Felten <Markus.Felten@gmx.de>
2020-07-07 13:49:31 +0200

    fix(pacman): compress all files (even in sub dirs)

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -1,20 +1,20 @@
 
 post_install() {
-	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map *.svg)
+	(cd {{installdir}}; find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
 	systemctl reload nginx
 }
 
 pre_upgrade() {
-	(cd {{installdir}}; rm *.gz)
+	(cd {{installdir}}; find -type f -name "*.gz" -print0|xargs -0 rm)
 }
 
 post_upgrade() {
-	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map *.svg)
+	(cd {{installdir}}; find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
 	systemctl reload nginx
 }
 
 pre_remove() {
-	(cd {{installdir}}; rm *.gz)
+	(cd {{installdir}}; find -type f -name "*.gz" -print0|xargs -0 rm)
 }
 
 post_remove() {
