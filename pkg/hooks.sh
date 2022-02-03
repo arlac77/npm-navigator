f906bf534bc73a07a8b99c018b682690e3f9d256
Markus Felten <Markus.Felten@gmx.de>
2022-02-03 21:25:06 +0100

    add missing pkg/hooks.sh from template

---
+++ pkg/hooks.sh
@@ -0,0 +1,22 @@
+
+post_install() {
+	(cd {{installdir}} && find -type f ! -name "*.gz" ! -name "*.png" -print0|xargs -0 gzip -k -9)
+	systemctl reload nginx
+}
+
+pre_upgrade() {
+	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 rm)
+}
+
+post_upgrade() {
+	(cd {{installdir}} && find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
+	systemctl reload nginx
+}
+
+pre_remove() {
+	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 rm)
+}
+
+post_remove() {
+	systemctl reload nginx
+}
