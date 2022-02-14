b8dbff89e9069ecc546fd574ef881bd5eb7e4c0c
Markus Felten <Markus.Felten@gmx.de>
2022-02-14 13:09:11 +0100

    add missing src/pages/AddCategory.svelte from template

---
+++ src/pages/AddCategory.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { CommandButton } from "svelte-command";
+  import CategoryCard from "../components/CategoryCard.svelte";
+  import { Category } from "../category.mjs";
+  import { session } from "../util.mjs";
+
+  const category = new Category();
+  let valid;
+
+  const command = category.saveCommand;
+
+  $: {
+    command.disabled = !valid || !session.hasEntitlement("konsum.category.add");
+  }
+</script>
+
+<h1>New Category</h1>
+
+<form>
+  <CategoryCard {category} bind:valid />
+  <CommandButton {command}/>
+</form>
