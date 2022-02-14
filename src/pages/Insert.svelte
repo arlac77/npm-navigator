98a816b6c5537f699a3cb11c4dada3e241275493
Markus Felten <Markus.Felten@gmx.de>
2022-02-14 13:09:23 +0100

    add missing src/pages/Insert.svelte from template

---
+++ src/pages/Insert.svelte
@@ -0,0 +1,14 @@
+<script>
+  import CategoryInsert from "../components/CategoryInsert.svelte";
+
+  export let router;
+  
+  const route = router.route;
+  const categories = $route.value;
+</script>
+
+<form>
+  {#each categories as category}
+    <CategoryInsert {category}/>
+  {/each}
+</form>
