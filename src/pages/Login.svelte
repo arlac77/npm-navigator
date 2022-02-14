38387f20d2be198371b3172029313b22b912dfe7
Markus Felten <Markus.Felten@gmx.de>
2022-02-14 13:09:25 +0100

    add missing src/pages/Login.svelte from template

---
+++ src/pages/Login.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Login } from "svelte-session-manager";
+  import { Modal } from "svelte-common";
+  import { session } from "../util.mjs";
+  import { api } from "../constants.mjs";
+
+  export let router;
+</script>
+
+<Modal close={() => router.abort('/')}>
+  <Login
+    {session}
+    endpoint={api + '/authenticate'}
+    result={() => router.continue('/')} />
+</Modal>
