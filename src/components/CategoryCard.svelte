0d1b0110a7aceefc7985f2b76a894db323cbd435
Markus Felten <Markus.Felten@gmx.de>
2022-02-14 13:08:56 +0100

    add missing src/components/CategoryCard.svelte from template

---
+++ src/components/CategoryCard.svelte
@@ -0,0 +1,65 @@
+<script>
+  import imask from "../imask.mjs";
+
+  export let category;
+  export let valid = false;
+
+  $: {
+    valid =
+      category.name &&
+      category.name.length &&
+      category.description &&
+      category.description.length &&
+      category.unit &&
+      category.unit.length &&
+      category.fractionalDigits;
+  }
+
+  function accept({ detail: maskRef }) {
+    category.fractionalDigits = maskRef.value;
+  }
+</script>
+
+<label for="name">
+  Name
+  <input
+    id="name"
+    type="text"
+    placeholder="Name"
+    size="20"
+    required
+    bind:value={category.name} />
+</label>
+
+<label for="description">
+  Description
+  <input
+    id="description"
+    type="text"
+    placeholder="Description"
+    size="20"
+    required
+    bind:value={category.description} />
+</label>
+<label for="unit">
+  Unit
+  <input
+    id="unit"
+    type="text"
+    placeholder="Unit"
+    size="20"
+    required
+    bind:value={category.unit} />
+</label>
+
+<label for="fractionalDigits">
+  Fractional digits
+  <input
+    id="fractionalDigits"
+    type="text"
+    placeholder="Fractional Digits"
+    size="20"
+    required
+    use:imask={{ mask: Number, scale: 0, signed: false, min: 0, max: 5 }}
+    bind:value={category.fractionalDigits} />
+</label>
